2241429fafd11f4d35f0b7c71e95d58f5a825082
Cathrinaaa <fritztacanay0907@gmail.com>
2025-10-07 20:07:34 +0800

    first commit

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
+// db.js
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import * as schema from "../shared/schema.js"; // <-- go up one folder, into shared
-import "dotenv/config";
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // required for Neon
-  },
 });
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(pool);
